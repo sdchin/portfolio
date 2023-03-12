dffb0005628d9b3360b46cc3eea513f621e18c7a
Steven Chin <chin.steven10@gmail.com>
2023-03-12 16:05:12 -0700

    Implement ButtonCount

--- hw5/ButtonCount.js
+++ hw5/ButtonCount.js
@@ -1,6 +1,31 @@
 class ButtonCount extends HTMLElement {
+  #count = 0;
+  #output;
+
   constructor() {
     super();
+
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    const style = `
+    <style>
+    button {
+      background-color: rebeccapurple;
+    }
+    </style>
+    `;
+    const markup = `
+    <button>Times clicked: <output>${this.#count}</output></button>
+    `;
+    shadowRoot.innerHTML = style + markup;
+
+    this.#output = shadowRoot.querySelector('output');
+
+    shadowRoot.querySelector('button').addEventListener('click', this.#increment.bind(this));
+  }
+
+  #increment() {
+    this.#count++;
+    this.#output.textContent = this.#count;
   }
 }
 
