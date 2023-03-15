eea5db44d6ad999802c39e8ed16c350880f08aea
Steven Chin <chin.steven10@gmail.com>
2023-03-14 19:38:39 -0700

    Add web component styling

--- hw5/ButtonCount.js
+++ hw5/ButtonCount.js
@@ -9,7 +9,37 @@ class ButtonCount extends HTMLElement {
     const style = `
     <style>
     button {
-      background-color: rebeccapurple;
+      border: none;
+      border-radius: 10px;
+      box-shadow: 4px 4px 10px #666;
+      background: linear-gradient(#e66465, #9198e5);
+      color: #fff;
+      font-family: 'Assistant', sans-serif;
+      font-size: 1.1rem; 
+      height: 50px;
+      margin: 10px;
+      position: relative;
+      transition: transform 200ms;
+      width: 150px;
+    }
+
+    button::after {
+      border-radius: 10px 10px 0 0;
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 50%;
+      background: linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.2));
+    }
+
+    button:hover {
+      transform: scale(0.95, 0.95);
+    }
+
+    button:active {
+      transform: scale(0.85, 0.85);
     }
     </style>
     `;
